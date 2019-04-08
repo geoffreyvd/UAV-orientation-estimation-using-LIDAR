c87af3e4fc7897a08385e3f6513a69426f952665
Geoffrey v D <geoffrey.v.d@live.nl>
2019-04-08 23:12:59 +0200

    cleaning and dividing code between classes

---
+++ examples/lidarVisualiser.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+
+# Arbitrary display params
+DISPLAY_CANVAS_SIZE_PIXELS  = 980
+DISPLAY_CANVAS_COLOR        = 'black'
+DISPLAY_SCAN_LINE_COLOR     = 'yellow'
+DISPLAY_SCAN_LINE_COLOR_LARGEST_DISTANCE     = 'red'
+DISPLAY_SCAN_LINE_COLOR_EXTRACTED_LINES     = 'blue'
+DISPLAY_SCAN_LINE_COLOR_PERPENDICULAR     = 'green'
+
+# URG-04LX specs
+URG_MAX_SCAN_DIST_MM        = 2000
+URG_DETECTION_DEG           = 240
+URG_SCAN_SIZE               = 682
+from sys import exit, version
+from math import sin, cos, radians, atan, atan2, pi, fabs, sqrt
+
+if version[0] == '3':
+    import tkinter as tk
+else:
+    import Tkinter as tk
+
+class lidarVisualiser(tk.Frame):
+    ""
+
+    def __init__(self):
+
+        # Create the frame        
+        tk.Frame.__init__(self, borderwidth = 4, relief = 'sunken')
+        self.master.geometry(str(DISPLAY_CANVAS_SIZE_PIXELS)+ "x" + str(DISPLAY_CANVAS_SIZE_PIXELS))
+        self.master.title('Hokuyo URG04LX  [ESC to quit]')
+        self.grid()
+        self.master.rowconfigure(0, weight = 1)
+        self.master.columnconfigure(0, weight = 1)
+        self.grid(sticky = tk.W+tk.E+tk.N+tk.S)
+        self.background = DISPLAY_CANVAS_COLOR
+
+        self.half_canvas_pix = DISPLAY_CANVAS_SIZE_PIXELS / 2
+        self.renderScale = self.half_canvas_pix / float(URG_MAX_SCAN_DIST_MM)
+
+        #pre calculate some value for plotting and mathss
+        scan_angle_rad = [radians(-URG_DETECTION_DEG/2 + (float(k)/URG_SCAN_SIZE) * \
+                                   URG_DETECTION_DEG) for k in range(URG_SCAN_SIZE)]
+        self.scanRadians = scan_angle_rad
+        self.scanAngles = [scan_angle_rad[k]*180/pi for k in range(URG_SCAN_SIZE)]
+
+        self.cos = [-cos(angle) * self.renderScale for angle in scan_angle_rad]
+        self.sin = [ sin(angle) * self.renderScale for angle in scan_angle_rad]
+        self.cosRaw = [ cos(angle) for angle in scan_angle_rad]
+        self.sinRaw = [ sin(angle) for angle in scan_angle_rad]    
+
+        self.showCount = 0
+
+        # Add a canvas for drawing
+        self.canvas =  tk.Canvas(self, \
+            width = DISPLAY_CANVAS_SIZE_PIXELS, \
+            height = DISPLAY_CANVAS_SIZE_PIXELS,\
+            background = DISPLAY_CANVAS_COLOR)
+        self.canvas.grid(row = 0, column = 0,\
+                    rowspan = 1, columnspan = 1,\
+                    sticky = tk.W+tk.E+tk.N+tk.S)
+
+        self.perpendicularLineCount = 0
+        self.extractedLinesCountDebug = 0
+
+        # Add scan lines to canvas, to be modified later
+        self.linesLidar = [self.canvas.create_line(\
+                         self.half_canvas_pix, \
+                         self.half_canvas_pix, \
+                         self.half_canvas_pix + self.sin[k] * URG_MAX_SCAN_DIST_MM,\
+                         self.half_canvas_pix + self.cos[k] * URG_MAX_SCAN_DIST_MM)
+                         for k in range(URG_SCAN_SIZE)]
+        self.linesExtracted = [self.canvas.create_line(\
+                         0, \
+                         0, \
+                         self.sin[k] * 10,\
+                         self.cos[k] * 10)
+                         for k in range(URG_SCAN_SIZE)]
+        self.perpendicularLines = [self.canvas.create_line(\
+                         0, \
+                         0, \
+                         self.sin[k] * 10,\
+                         self.cos[k] * 10)
+                         for k in range(URG_SCAN_SIZE)]
+
+        [self.canvas.itemconfig(line, fill=DISPLAY_SCAN_LINE_COLOR) for line in self.linesLidar]
+        print("origin x and y: {}, renderScale: {}".format(self.half_canvas_pix, self.renderScale))
+
+    def plotScanDataPointsAsLines(self, scandata):
+        # Modify the yellow displayed lines according to the current scan
+        [self.canvas.coords(self.linesLidar[k], 
+                            self.half_canvas_pix, \
+                            self.half_canvas_pix, \
+                            self.half_canvas_pix + self.sin[k] * scandata[k],\
+                            self.half_canvas_pix + self.cos[k] * scandata[k]) \
+         for k in range(len(scandata))]
+        self.showCount += 1   
+
+    def plotWalls(self, walls):        
+        for idx, w in enumerate(walls):
+            print("idx: {}, point 1 idx: {}, point 2 idx: {}".format(idx, w.index1, w.index2))
+            self.canvas.coords(self.linesExtracted[idx], w.x1, w.y1, w.x2, w.y2)
+            widthWall = 1 + 10/(idx +1)
+            self.canvas.itemconfig(self.linesExtracted[idx], fill=DISPLAY_SCAN_LINE_COLOR_EXTRACTED_LINES, width=widthWall) 
+    
+    def plotPerpendicularLines(self, perpendicularDistanceRaw, perpendicularRadian):
+        perpendicularDistance = perpendicularDistanceRaw * self.renderScale
+        perpendicularLineEndX = self.half_canvas_pix+ perpendicularDistance * sin(perpendicularRadian)
+        perpendicularLineEndY = self.half_canvas_pix+ perpendicularDistance * -cos(perpendicularRadian)
+        self.perpendicularLineCount += 1
+        [self.canvas.coords(self.perpendicularLines[self.perpendicularLineCount], 
+                        perpendicularLineEndX, \
+                        perpendicularLineEndY, \
+                        self.half_canvas_pix,\
+                        self.half_canvas_pix)]    
+        [self.canvas.itemconfig(self.perpendicularLines[self.perpendicularLineCount], fill=DISPLAY_SCAN_LINE_COLOR_PERPENDICULAR)]
+
+    def plotSplitLine(self, firstPointX, firstPointY, lastPointX, lastPointY):
+        self.extractedLinesCountDebug+=1
+        [self.canvas.coords(self.linesExtracted[self.extractedLinesCountDebug], 
+                        firstPointX, \
+                        firstPointY, \
+                        lastPointX,\
+                        lastPointY)]    
+        [self.canvas.itemconfig(self.linesExtracted[self.extractedLinesCountDebug], fill=DISPLAY_SCAN_LINE_COLOR_EXTRACTED_LINES)]
+
+    def plotLargestDistance(self, indexLargestDistance):
+        [self.canvas.itemconfig(self.linesLidar[indexLargestDistance], fill=DISPLAY_SCAN_LINE_COLOR_LARGEST_DISTANCE)]
+        
+    def updateGUI(self):
+        self.update()
+    
+    def getShowCount(self):
+        return self.showCount
--- examples/mockLIDAR.py
+++ examples/mockLIDAR.py
@@ -1,28 +1,19 @@
 #!/usr/bin/env python3
 URG_DEVICE                  = '/dev/ttyACM0'
 
-# URG-04LX specs
-URG_MAX_SCAN_DIST_MM        = 4000
-URG_DETECTION_DEG           = 240
-URG_SCAN_SIZE               = 682
-
 LOG_END_OF_DISTANCE = 10001
 LOG_END_OF_SCAN = 10002
 READ_FROM_SERIAL = 1
 READ_FROM_FILE = 2
 
-
 from breezylidar import URG04LX
 
-from math import sin, cos, radians
 from time import time, sleep, ctime
 from sys import exit, version
 
 if version[0] == '3':
-    import tkinter as tk
     import _thread as thread
 else:
-    import Tkinter as tk
     import thread
 
 # Runs on its own thread
@@ -31,7 +22,7 @@ def grab_scan(obj):
         scandata = obj.lidar.getScan()
         if scandata:
             obj.scandata = scandata
-            obj.count += 1
+            obj.scanCount += 1
             sleep(.01) # pause a tiny amount to allow following check to work
             if not obj.running:
                 break
@@ -47,6 +38,7 @@ class URGMocker():
         self.bytesFromLIDAR = []
         # No scan data to start
         self.scandata = []
+        self.scanCount = 0
         self.running = True
 
         self.readFrom = readFrom
@@ -55,34 +47,18 @@ class URGMocker():
             self.file = open('urg04-LX-log', 'rb') 
             self.bytesFromLIDAR = self.file.read()
             self.scanIndex = 0         
-        else:
+        elif readFrom == READ_FROM_SERIAL:
             # Create a URG04LX object and connect to it
             self.lidar = URG04LX(URG_DEVICE)
             thread.start_new_thread( grab_scan, (self,) ) 
-        
-    def run(self):
-        '''
-        Call this when you're ready to run.
-        '''
-        ##write to log file
-        # self.file = open('urg04-LX-log', 'wb') 
-        # while 1:
-        #     self.readScanFromLidarAndWriteToFile()
-        
-    # Runs on its own thread
-    def readScanFromLidarAndWriteToFile(self):
-        while True:
-            scandata = self.lidar.getScan()
-            if scandata:
-                self.scandata = scandata
-                for distance in scandata:
-                    distanceByte = distance.to_bytes(2, byteorder='big')
-                    self.file.write(distanceByte)
-                    self.file.write(LOG_END_OF_DISTANCE.to_bytes(2, byteorder='big'))
-                self.file.write(LOG_END_OF_SCAN.to_bytes(2, byteorder='big'))
-                sleep(.01) # pause a tiny amount to allow following check to work
-                if not self.running:
-                    break
+        else:
+            self.lidar = URG04LX(URG_DEVICE)
+            self.readScanFromLidarAndWriteToFile()
+            
+    def _quit(self):
+        del self.lidar
+        exit(0)
+     
     def getScan(self):
         if self.readFrom == READ_FROM_FILE:
             scandata = []
@@ -102,11 +78,27 @@ class URGMocker():
             return scandata
         else:
             return self.scandata
+
+    def getCount(self):
+        return self.scanCount
+
+    def readScanFromLidarAndWriteToFile(self):
+        #write to log file
+        self.file = open('urg04-LX-log1', 'wb') 
+        while True:
+            scandata = self.lidar.getScan()
+            if scandata:
+                self.scandata = scandata
+                for distance in scandata:
+                    distanceByte = distance.to_bytes(2, byteorder='big')
+                    self.file.write(distanceByte)
+                    self.file.write(LOG_END_OF_DISTANCE.to_bytes(2, byteorder='big'))
+                self.file.write(LOG_END_OF_SCAN.to_bytes(2, byteorder='big'))
+                sleep(.01) # pause a tiny amount to allow following check to work
+                if not self.running:
+                    break
         
-        
-# Instantiate and pop up the window
+# Instantiate
 if __name__ == '__main__':
     
-    mocker = URGMocker(READ_FROM_SERIAL)
-    
-    mocker.run()
+    mocker = URGMocker(3)
