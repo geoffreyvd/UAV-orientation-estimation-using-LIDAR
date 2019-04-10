d53c17e6e8d742ac8fa903567905d2995d436745
Geoffrey v D <geoffrey.v.d@live.nl>
2019-04-10 09:58:42 +0200

    removing old files

--- examples/testLinearRegression.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-from splitAndMerge import linearRegression
-
-listX = [3,4,5,6,7,8,9]
-listY = [7,7,11,11,15,16,19]
-perpendicularRadian, perpendicularDistance =  linearRegression(listX, listY)
-print("radian {}, distance: {}".format(perpendicularRadian, perpendicularDistance))
